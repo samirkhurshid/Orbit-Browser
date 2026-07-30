f162dcc5c984eaef57d09063c10a5d31912fbdf2
Orbit Developer <orbit.browser@developer.local>
2026-07-30 22:11:37 +0530

    Clear webview back history stack when opening new site from homepage

--- app/src/main/java/com/Orbit/browser/ui/screens/BrowserScreen.kt
+++ app/src/main/java/com/Orbit/browser/ui/screens/BrowserScreen.kt
@@ -279,6 +279,7 @@ fun PersistentWebViewStack(
                 is BrowserCommand.Refresh   -> activeWv?.reload()
                 is BrowserCommand.LoadUrl   -> {
                     if (command.clearHistory) {
+                        activeWv?.shouldClearHistoryOnNextLoad = true
                         activeWv?.clearHistory()
                     }
                     activeWv?.loadUrl(command.url)
